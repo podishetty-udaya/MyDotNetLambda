103fb90b48e45a14ea25f4ebcb821be6ee375079
Udaya2506 <sonylaxmi2506@gmail.com>
2026-02-23 20:57:59 -0800

    added

---
+++ src/MyLambdaProject/Function.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace MyLambdaProject
+{
+    public class Function
+    {
+        public string Handler(string input, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Received input: {input}");
+            return $"Hello from Lambda! You sent: {input}";
+        }
+    }
+}
